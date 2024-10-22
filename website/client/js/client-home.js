a1fba18f3ab7b36a951dac097bd31dc64a110658
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-10-22 15:22:02 +0800

    Fixed the bug in client home

--- website/client/js/client-home.js
+++ website/client/js/client-home.js
@@ -8,7 +8,12 @@ function getData() {
             'Content-Type': 'application/json',
         }
     })
-    .then(response => response.json())  
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(data => {
         if (data.error) {
             console.error('Error:', data.error);
