9e8ba0127bfc87fff45804aa1af339c4a2442aab
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-11-04 18:19:50 +0800

    template of the product routes and controllers

--- backend/nodejs/controllers/productControllers.js
+++ backend/nodejs/controllers/productControllers.js
@@ -1,8 +1,141 @@
-//sample code
+// Implementation of the logic
 
-//implementation of the logic
+/*
+ * Gets the product for a certain booth
+ */
 const getProducts = async (request, response) => {
+    const db = request.db;
+    const { boothId } = request.params;
 
-}
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM products WHERE boothId = ?', [boothId], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results); 
+                }
+            });
+        });
 
-export { getProducts }
+        response.json(results);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
+
+/*
+ * Gets the detailed version of the product 
+ */
+const getProductDetails = async (request, response) => {
+    const db = request.db;
+    const { productId } = request.params;
+
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('THIS IS WHERE THE QUERY FOR GETTING THE PRODUCT DETAILS', [productId], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results); 
+                }
+            });
+        });
+
+        response.status(200).json(results);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
+
+/*
+ * Decrements the stock of the product given.
+ */
+const buyProduct = async (request, response) => {
+    const db = request.db;
+    const { productId } = request.params;
+
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM products WHERE boothId = ?', [productId], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results); 
+                }
+            });
+        });
+
+        response.json(results);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
+
+/**
+ * Create product
+ */
+const createProduct = async (request, response) => {
+    const db = request.db;
+    const { productId } = request.params;
+
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM products WHERE boothId = ?', [productId], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results); 
+                }
+            });
+        });
+
+        response.json(results);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
+
+const editProduct = async (request, response) => {
+    const db = request.db;
+    const { productId } = request.params;
+
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM products WHERE boothId = ?', [productId], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results); 
+                }
+            });
+        });
+
+        response.json(results);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
+
+const changeStatusProduct = async (request, response) => {
+    const db = request.db;
+    const { productId } = request.params;
+
+    try {
+        const results = await new Promise((resolve, reject) => {
+            db.query('SELECT * FROM products WHERE boothId = ?', [productId], (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results); 
+                }
+            });
+        });
+
+        response.json(results);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+};
+
+export { getProducts, getProductDetails, buyProduct, createProduct, editProduct, changeStatusProduct };
--- backend/nodejs/routes/productRoutes.js
+++ backend/nodejs/routes/productRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getProducts } from "../controllers/productControllers";
+import { getProducts, getProductDetails, buyProduct, createProduct, editProduct, changeStatusProduct } from "../controllers/productControllers";
 
 const router = express.Router();
 
-router.get("/", getProducts);
+router.get("/:boothId", getProducts);
+router.get("details/:productId", getProductDetails);
+router.patch("/:productId", buyProduct);
+router.post("/create", createProduct)
+router.patch("/edit/:productId", editProduct);
+router.patch("/status/:productId", changeStatusProduct);
 
 export default router;
