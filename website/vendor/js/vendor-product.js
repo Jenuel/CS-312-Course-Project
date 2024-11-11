a668d9945fa91f2a83d275eda379ba89cecaed2e
neilcayton <neilcayton03@gmail.com>
2024-11-11 23:48:05 +0800

    vendor js: DOM implementation

--- website/vendor/js/vendor-product.js
+++ website/vendor/js/vendor-product.js
@@ -19,7 +19,7 @@ function showAddProductForm() {
                 <label for="status">Status</label>
                 <select id="status">
                     <option>Live</option>
-                    <option>Unpublished</option>
+                    <option>Pending</option>
                 </select>
             </div>
             
@@ -54,6 +54,58 @@ function showAddProductForm() {
     const overlay = modalContainer.querySelector('.modal-overlay');
 
     cancelBtn.addEventListener('click', () => modalContainer.remove());
-    overlay.addEventListener('click', () => modalContainer.remove());
-    submitBtn.addEventListener('click', handleProductSubmit);
+    // overlay.addEventListener('click', () => modalContainer.remove());
+    submitBtn.addEventListener('click', () => {
+        createProduct();  
+        modalContainer.remove()
+    });
+}
+
+
+function showProductDetail(dataForm) {
+    console.log(dataForm.ProductName.value)
+
+    // array.forEach(dataForm => {
+        const productDetailContainer = document.createElement('div');
+        productDetailContainer.className = "product-detail-container";
+        productDetailContainer.innerHTML = `
+             <li>${dataForm.ProductName}</li>
+                            <li>${dataForm.ProductStatus}</li>
+                            <li>${dataForm.ProductPrice}</li>
+                            <li>${dataForm.ProductStock}</li>
+                            <li>${dataForm.ProductSales}</li>
+                            <li>
+                                    <button type="button" class="p-edit-button">
+                                        <i class='bx bx-edit'></i>
+                                    </button>
+    
+                                    <button type="button" class="p-delete-button">
+                                        <i class='bx bx-trash-alt' ></i>
+                                    </button>
+                                
+                            </li>`
+    
+        const editBtn = productDetailContainer.querySelector('p-edit-button');
+        var deleteBtn = productDetailContainer.querySelector('p-delete-button');
+        deleteBtn = document.createElement('button')
+        deleteBtn.addEventListener("click", () => productDetailContainer.remove() )
+
+        tableBody.appendChild(productDetailContainer)
+    // });
+
+}
+
+
+function createProduct() {
+    const inputProdDom = document.querySelector(".add-container")
+
+    const data =  {
+        ProductName: inputProdDom.querySelector('#product-name').value,
+        ProductStatus: inputProdDom.querySelector('#status').value,
+        ProductPrice: inputProdDom.querySelector('#price').value,
+        ProductStock: inputProdDom.querySelector('#stock').value || null,
+        ProductSales: null
+    };
+
+    showProductDetail(data);
 }
