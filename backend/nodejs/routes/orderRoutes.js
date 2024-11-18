5c1c67c2360051a81d22f873c9a2af203dc8418f
Renuel Jeremi Balogo <renueljeremibalogo@gmail.com>
2024-11-19 01:20:08 +0800

    Fixed possible conflicting routes

--- backend/nodejs/routes/orderRoutes.js
+++ backend/nodejs/routes/orderRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getPendingOrders, getCompletedOrders, createOrder, cancelOrder, approveOrder, approveCancellation } from "../controllers/orderControllers";
+import { getPendingOrders, getCompletedOrders, createOrder, cancelOrder, approveOrder, approveCancellation } from "../controllers/orderControllers.js";
 const router = express.Router();
 
-router.get("/:boothId", getPendingOrders);
-router.get("/:boothId", getCompletedOrders);
+router.get("/pending", getPendingOrders);
+router.get("/complete/:boothId", getCompletedOrders);
 router.post("/create", createOrder);
 router.patch("/cancel/:orderId", cancelOrder);
-router.patch("/approve", approveOrder);
+router.patch("/approve/:id", approveOrder);
 router.delete("/cancel/:orderId");
 
 export default router;
--- backend/nodejs/routes/productRoutes.js
+++ backend/nodejs/routes/productRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getProducts, getProductDetails, buyProduct, createProduct, editProduct, changeStatusProduct } from "../controllers/productControllers";
+import { getProducts, getProductDetails, buyProduct, createProduct, editProduct, changeStatusProduct } from "../controllers/productControllers.js";
 
 const router = express.Router();
 
 router.get("/:boothId", getProducts);
 router.get("details/:productId", getProductDetails);
-router.patch("/:productId", buyProduct);
+router.patch("/buy/:productId", buyProduct);
 router.post("/create", createProduct)
 router.patch("/edit/:productId", editProduct);
 router.patch("/status/:productId", changeStatusProduct);
