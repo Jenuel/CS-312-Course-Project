1aa6b2a8aed50526a3c5a38021c1ae890a69fce6
neilcayton <neilcayton03@gmail.com>
2024-12-15 14:21:17 +0800

    debug fix

--- backend/nodejs/controllers/productControllers.js
+++ backend/nodejs/controllers/productControllers.js
@@ -10,7 +10,7 @@ const getProducts = async (request, response) => {
   const db = request.db;
   const { boothId } = request.params;
   const {
-    query: { sort},
+    query: { sort },
   } = request;
 
   try {
@@ -36,7 +36,6 @@ const getProducts = async (request, response) => {
     const [rows] = await db.query(query, params);
 
     response.json(rows); // Convert response to JSON
-
   } catch (error) {
     console.error("Error fetching products:", error);
     response.status(500).send("Failed to fetch products");
@@ -47,12 +46,12 @@ const getLiveProducts = async (request, response) => {
   const db = request.db;
   const { boothId } = request.params;
   const {
-    query: { filter, sort},
+    query: { filter, sort },
   } = request;
 
   try {
     let query =
-      'SELECT p.ProductID AS "ProductID", p.name AS "Name", p.StocksRemaining AS "Stocks", p.Price AS "Price", p.status AS "Status", TO_BASE64(p.Image) AS "Image" FROM `p roduct` p WHERE p.BoothID = ? AND p.status = "Live"';
+      'SELECT p.ProductID AS "ProductID", p.name AS "Name", p.StocksRemaining AS "Stocks", p.Price AS "Price", p.status AS "Status", TO_BASE64(p.Image) AS "Image" FROM `product` p WHERE p.BoothID = ? AND p.status = "Live"';
     let params = [boothId];
 
     // Apply sorting logic
@@ -73,15 +72,11 @@ const getLiveProducts = async (request, response) => {
     const [rows] = await db.query(query, params);
 
     response.json(rows); // Convert response to JSON
-
   } catch (error) {
     console.error("Error fetching products:", error);
     response.status(500).send("Failed to fetch products");
   }
 };
-
-
-
 
 /*
  Gets the detailed version of the product
@@ -137,27 +132,25 @@ const buyProduct = async (request, response) => {
     );
 
     if (allPositive.length && allPositive[0].AllPositive) {
+      await db.query(
+        "UPDATE `order` SET `Status` = 'Pending', `DatePaid` = ? WHERE `order`.`OrderID` = ?",
+        [datePaid, orderId]
+      ); // updated status and date of order
 
       await db.query(
-      "UPDATE `order` SET `Status` = 'Pending', `DatePaid` = ? WHERE `order`.`OrderID` = ?",
-      [datePaid,orderId]
-    );// updated status and date of order
+        "UPDATE product p SET p.StocksRemaining = p.StocksRemaining - ( SELECT o.Quantity FROM order_products o WHERE o.OrderID = ? AND o.ProductID = p.ProductID ) WHERE p.ProductID IN ( SELECT o.ProductID FROM order_products o WHERE o.OrderID = ? )",
+        [orderId, orderId]
+      ); // remove stocks in product
 
       await db.query(
-      "UPDATE product p SET p.StocksRemaining = p.StocksRemaining - ( SELECT o.Quantity FROM order_products o WHERE o.OrderID = ? AND o.ProductID = p.ProductID ) WHERE p.ProductID IN ( SELECT o.ProductID FROM order_products o WHERE o.OrderID = ? )",
-      [orderId, orderId]
-    ); // remove stocks in product
+        "INSERT INTO inventory (InventoryID, ProductID, Date, Type, Quantity) SELECT NULL, p.ProductID, DATE(o.DatePaid) AS DatePaid, 'out', p.Quantity FROM `order` o JOIN order_products p ON o.OrderID = p.OrderID WHERE o.OrderID = ?",
+        [orderId]
+      ); // insert updated in inventory
 
-    
-    await db.query(
-      "INSERT INTO inventory (InventoryID, ProductID, Date, Type, Quantity) SELECT NULL, p.ProductID, DATE(o.DatePaid) AS DatePaid, 'out', p.Quantity FROM `order` o JOIN order_products p ON o.OrderID = p.OrderID WHERE o.OrderID = ?",
-      [orderId]
-    );// insert updated in inventory
-
-    const [breakdown]= await db.query(
-      "SELECT o.OrderID AS 'order id', x.name AS 'product name', x.Price AS 'product price', p.Quantity AS 'number of ordered product', p.Total AS 'total price per product', o.Price AS 'overall total' FROM `order` o JOIN `order_products` p ON o.OrderID = p.OrderID JOIN `product` x ON p.ProductID = x.ProductID JOIN `booth` b ON b.BoothID = x.BoothID WHERE o.OrderID = ?",
-      [orderId]
-    );// return breakdown order
+      const [breakdown] = await db.query(
+        "SELECT o.OrderID AS 'order id', x.name AS 'product name', x.Price AS 'product price', p.Quantity AS 'number of ordered product', p.Total AS 'total price per product', o.Price AS 'overall total' FROM `order` o JOIN `order_products` p ON o.OrderID = p.OrderID JOIN `product` x ON p.ProductID = x.ProductID JOIN `booth` b ON b.BoothID = x.BoothID WHERE o.OrderID = ?",
+        [orderId]
+      ); // return breakdown order
       response.json(breakdown);
     } else {
       response.json({
@@ -169,7 +162,6 @@ const buyProduct = async (request, response) => {
     response.status(500).send("Failed to buy products");
   }
 };
-
 
 /**
  * Create product
@@ -190,33 +182,31 @@ const createProduct = async (request, response) => {
       .send('Invalid status. It must be either "active" or "inactive".');
   }
   try {
-
     console.log("Received image data length:", image ? image.length : 0);
 
     await db.beginTransaction();
 
     let imageBuffer = null;
-    if (image && typeof image === 'string') {
-      const base64Data = image.replace(/^data:image\/\w+;base64,/, '');
-      imageBuffer = Buffer.from(base64Data, 'base64');
+    if (image && typeof image === "string") {
+      const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+      imageBuffer = Buffer.from(base64Data, "base64");
     }
 
     const [rows] = await db.query(
-      'INSERT INTO `product` (`ProductID`, `BoothID`, `StocksRemaining`, `Price`, `name`, `status`, `Image`) VALUES (NULL, ?,?, ?, ?, ?, ?)',
+      "INSERT INTO `product` (`ProductID`, `BoothID`, `StocksRemaining`, `Price`, `name`, `status`, `Image`) VALUES (NULL, ?,?, ?, ?, ?, ?)",
       [boothID, stocks, price, name, status, imageBuffer]
-    );// query for creating new product
+    ); // query for creating new product
 
     await db.query(
       'INSERT INTO `inventory` (`InventoryID`, `ProductID`, `Date`, `Type`, `Quantity`) VALUES (NULL, ?, ?, "in", ?)',
       [rows.insertId, getCurrentDate(), stocks]
-    );// query to add stocks to inventory
+    ); // query to add stocks to inventory
 
     response.json(rows);
   } catch (error) {
     console.error("Error creating products:", error);
     response.status(500).send("Failed to create products");
   }
-
 };
 
 /**
@@ -226,29 +216,25 @@ const createProduct = async (request, response) => {
 USED BY: vendor 
 */
 const editProduct = async (request, response) => {
-
   const db = request.db;
-  const { productId } = request.params; 
+  const { productId } = request.params;
   const { product } = request.body; // product is an array of "key:value"
 
   try {
-
     for (const field of product) {
-      const key = Object.keys(field)[0];  // arraylist for keys from body, [name, price] 
+      const key = Object.keys(field)[0]; // arraylist for keys from body, [name, price]
       let value = Object.values(field)[0]; // arraylist for values from body, [myName,150]
 
       if (key === "Image" && value) {
-        value = value.replace(/^data:image\/\w+;base64,/, '');
-        value = Buffer.from(value, 'base64');
+        value = value.replace(/^data:image\/\w+;base64,/, "");
+        value = Buffer.from(value, "base64");
       }
-
-
 
       if (key === "StocksRemaining") {
-        await updateStockInInventory(value, productId , db);
+        await updateStockInInventory(value, productId, db);
       }
 
-       // Prepare the query
+      // Prepare the query
       const query = `UPDATE \`product\` SET \`${key}\` = ? WHERE \`ProductID\` = ?`;
 
       // Execute the query
@@ -259,13 +245,13 @@ const editProduct = async (request, response) => {
           .status(404)
           .send(`Product with ID ${productId} not found or not updated`);
       }
-    }// end of loop
+    } // end of loop
 
     response.json({
       message: "Product updated successfully",
     });
   } catch (error) {
-    console.error("Error updating product:",  error.message, error.stack);
+    console.error("Error updating product:", error.message, error.stack);
     response.status(500).json({ error: error.message });
   }
 };
@@ -289,7 +275,7 @@ const deleteProduct = async (request, response) => {
 
     if (product.length === 0) {
       return response.status(404).json({
-        message: "Product not found"
+        message: "Product not found",
       });
     }
 
@@ -301,7 +287,8 @@ const deleteProduct = async (request, response) => {
 
     if (orders[0].orderCount > 0) {
       return response.status(400).json({
-        message: "Cannot delete product that has associated orders. Consider deactivating it instead."
+        message:
+          "Cannot delete product that has associated orders. Consider deactivating it instead.",
       });
     }
 
@@ -310,10 +297,7 @@ const deleteProduct = async (request, response) => {
 
     try {
       // Delete inventory records first
-      await db.query(
-        "DELETE FROM inventory WHERE ProductID = ?",
-        [productId]
-      );
+      await db.query("DELETE FROM inventory WHERE ProductID = ?", [productId]);
 
       // Delete the product
       const [deleteResult] = await db.query(
@@ -326,19 +310,17 @@ const deleteProduct = async (request, response) => {
 
       response.json({
         message: "Product deleted successfully",
-        deletedProductId: productId
+        deletedProductId: productId,
       });
-
     } catch (error) {
       await db.rollback();
       throw error;
     }
-
   } catch (error) {
     console.error("Error deleting product:", error);
     response.status(500).json({
       message: "Failed to delete product",
-      error: error.message
+      error: error.message,
     });
   }
 };
@@ -354,30 +336,30 @@ const updateStockInInventory = async (value, productId, db) => {
     // Query to get the current and updated stock values
     const [difference] = await db.query(
       'SELECT p.StocksRemaining as "current stocks", ' +
-      '(p.StocksRemaining - ? ) as "updated stocks" ' +
-      'FROM product p WHERE p.ProductID = ?',
+        '(p.StocksRemaining - ? ) as "updated stocks" ' +
+        "FROM product p WHERE p.ProductID = ?",
       [value, productId]
     );
 
-    const { 'current stocks': currentStocks, 'updated stocks': updatedStocks } = difference[0];
+    const { "current stocks": currentStocks, "updated stocks": updatedStocks } =
+      difference[0];
 
-    if (updatedStocks > 0) { // stocks decreased
+    if (updatedStocks > 0) {
+      // stocks decreased
       await db.query(
-        'INSERT INTO `inventory` (`InventoryID`, `ProductID`, `Date`, `Type`, `Quantity`) ' + 
-        'VALUES (NULL, ?, ?, "out", ?)',
+        "INSERT INTO `inventory` (`InventoryID`, `ProductID`, `Date`, `Type`, `Quantity`) " +
+          'VALUES (NULL, ?, ?, "out", ?)',
         [productId, getCurrentDate(), updatedStocks]
       ); // query to add stocks to inventory when stocks decrease
-
-    } else { // stocks increased
-      const stocks = (updatedStocks * -1);
+    } else {
+      // stocks increased
+      const stocks = updatedStocks * -1;
       await db.query(
-        'INSERT INTO `inventory` (`InventoryID`, `ProductID`, `Date`, `Type`, `Quantity`) ' + 
-        'VALUES (NULL, ?, ?, "in", ?)',
+        "INSERT INTO `inventory` (`InventoryID`, `ProductID`, `Date`, `Type`, `Quantity`) " +
+          'VALUES (NULL, ?, ?, "in", ?)',
         [productId, getCurrentDate(), stocks]
       ); // query to add stocks to inventory when stocks increase
-
     }
-
   } catch (error) {
     console.error("Error updating stock information:", error);
     throw new Error("Error updating stock information.");
@@ -402,11 +384,11 @@ const getCurrentDate = () => {
 export statement
 */
 export {
-      getProducts,
-      getLiveProducts,
-      getProductDetails,
-      buyProduct,
-      createProduct,
-      editProduct,
-      deleteProduct
-    };
+  getProducts,
+  getLiveProducts,
+  getProductDetails,
+  buyProduct,
+  createProduct,
+  editProduct,
+  deleteProduct,
+};
